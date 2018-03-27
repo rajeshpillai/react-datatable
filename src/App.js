ad585ccbfcee13033009d2c790c319c8020d1845
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-03-27 16:01:56 +0530

    House Keeping

--- src/App.js
+++ src/App.js
@@ -10,9 +10,15 @@ export default class App extends React.Component {
     this.state = {
       headers: [
         {title:"Id",accessor: "id", index: 0},
-        {title:"Name",accessor: "name", width: 300, index: 0},
-        {title:"Age",accessor: "age",index: 1},
-        {title:"Qualification",accessor: "qualification",index:2},
+        {title:"Profile",accessor:"profile", width: 80, index:1,cell:{
+          type: "image",
+          style: {
+            "width": "50px",
+          }
+        }},
+        {title:"Name",accessor: "name", width: 300, index: 2},
+        {title:"Age",accessor: "age",index: 3},
+        {title:"Qualification",accessor: "qualification",index:4},
         {title:"Rating",accessor: "rating",index:3,width: 200, cell: row => (
           <div  className="rating">
             <div style={{
@@ -23,17 +29,11 @@ export default class App extends React.Component {
            }}>{row}</div>
           </div>
         )},
-        {title:"Profile",accessor:"profile", width: 200, index:4,cell:{
-          type: "image",
-          style: {
-            "width": "50px",
-          }
-        }}
       ],
       data: [
           {id: 1, name:"a", age:29, qualification:"B.Com",rating:3,profile: "./img/img1.jpg"},
           {id: 2, name:"b", age:35, qualification:"B.Sc",rating:2,profile:"./img/img2.jpg"},
-          {id: 3, name:"c", age:42, qualification:"B.E",rating:3,profile:"./img/img3.jpeg"},
+          {id: 3, name:"c", age:42, qualification:"B.E",rating:3,profile:"./img/img2.jpg"},
         ]
     }
 
@@ -44,7 +44,8 @@ export default class App extends React.Component {
         name: "name " + i,
         age: i + 18,
         qualification: "Graduate",
-        rating: (i%2 ? 3 : 4)
+        rating: (i%2 ? 3 : 4),
+        profile: "./img/img2.jpg"
       })
     }
     this.state.data = [...this.state.data, ...data]
@@ -59,7 +60,7 @@ export default class App extends React.Component {
             keyField="id"
             pagination={{
               enabled: true,
-              pageLength: 3,
+              pageLength: 5,
               type: "long"
             }}
             width="600px"
